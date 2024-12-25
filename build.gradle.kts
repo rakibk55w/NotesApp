f555b80248a37e7b3f19e9532989a9f6dedd8471
rakib <rakibk55w@gmail.com>
2024-12-25 19:41:18 +0600

    All design done

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         google()
     }
     dependencies {
-        val navVersion = "2.8.5"
+        val navVersion = "2.7.5"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
     }
 }
@@ -14,6 +14,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     id ("com.android.library") version "7.3.1" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
-    id ("androidx.navigation.safeargs") version "2.8.5" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    id ("androidx.navigation.safeargs") version "2.7.5" apply false
 }
